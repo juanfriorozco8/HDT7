e4aabd8e02594d60d59ce2d57a51ad697ee78a03
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-03-27 21:23:44 -0600

    Main terminada

--- Main.java
+++ Main.java
@@ -1,3 +1,41 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        InventoryManager inventory = new InventoryManager();
+
+        // Cargar CSV
+        try (Scanner sc = new Scanner(new File("inventario_ropa_deportiva_30.csv"))) {
+            while (sc.hasNextLine()) {
+                String[] data = sc.nextLine().split(",");
+                String SKU = data[0];
+                String name = data[1];
+                String description = data[2];
+                Map<String, Integer> sizes = new HashMap<>();
+                String[] sizesData = data[3].split("\\|");
+                for (String sizeData : sizesData) {
+                    String[] sizeQty = sizeData.split(":");
+                    sizes.put(sizeQty[0], Integer.parseInt(sizeQty[1]));
+                }
+                Product p = new Product(SKU, name, description, sizes);
+                inventory.addProduct(p);
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading CSV: " + e.getMessage());
+        }
+
+        // Uso básico:
+        inventory.displayProductsBySKU();
+        inventory.displayProductsByName();
+
+        // Buscar un producto:
+        Product foundBySKU = inventory.findBySKU("123");
+        System.out.println("\nFound by SKU (123): " + foundBySKU);
+
+        Product foundByName = inventory.findByName("Chumpa impermeable");
+        System.out.println("\nFound by Name (Chumpa impermeable): " + foundByName);
+    }
 }
