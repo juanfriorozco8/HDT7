41191c42bc83b059103c37838685c8c509e0d0c7
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-03-27 21:24:30 -0600

    JUnits terminados

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -1,3 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class BinarySearchTreeTest {
-    
+    @Test
+    public void testInsertAndSearch() {
+        BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+        bst.insert(10, "Ten");
+        bst.insert(5, "Five");
+        bst.insert(15, "Fifteen");
+
+        assertEquals("Ten", bst.search(10));
+        assertEquals("Five", bst.search(5));
+        assertEquals("Fifteen", bst.search(15));
+        assertNull(bst.search(20));
+    }
 }
