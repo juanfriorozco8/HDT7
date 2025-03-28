a11e630a8ab0f8be2a0e7a57dd0fce433869d1ec
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-03-27 21:15:43 -0600

    clase Product terminada

--- Product.java
+++ Product.java
@@ -1,3 +1,25 @@
+import java.util.Map;
 public class Product {
-    
+    private String SKU;
+    private String name;
+    private String description;
+    private Map<String, Integer> sizes;
+
+    public Product(String SKU, String name, String description, Map<String, Integer> sizes) {
+        this.SKU = SKU;
+        this.name = name;
+        this.description = description;
+        this.sizes = sizes;
+    }
+
+    public String getSKU() { return SKU; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public Map<String, Integer> getSizes() { return sizes; }
+
+    @Override
+    public String toString() {
+        return SKU + " | " + name + " | " + description + " | " + sizes;
+    }
 }
+
