385f4c6e0111f456799d22d2192a06b21f749bc5
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-03-27 21:22:02 -0600

    InventoryManger clase terminada

--- InventoryManager.java
+++ InventoryManager.java
@@ -1,3 +1,27 @@
 public class InventoryManager {
-    
+    private BinarySearchTree<String, Product> bySKU = new BinarySearchTree<>();
+    private BinarySearchTree<String, Product> byName = new BinarySearchTree<>();
+
+    public void addProduct(Product product) {
+        bySKU.insert(product.getSKU(), product);
+        byName.insert(product.getName().toLowerCase(), product);
+    }
+
+    public Product findBySKU(String SKU) {
+        return bySKU.search(SKU);
+    }
+
+    public Product findByName(String name) {
+        return byName.search(name.toLowerCase());
+    }
+
+    public void displayProductsBySKU() {
+        System.out.println("Products ordered by SKU:");
+        bySKU.inOrderTraversal();
+    }
+
+    public void displayProductsByName() {
+        System.out.println("Products ordered by Name:");
+        byName.inOrderTraversal();
+    }
 }
